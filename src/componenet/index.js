647fd9407b5b2ed658ebc788f00677f28146feed
Madhucrj3 <madhureshcrj3@gmail.com>
2023-03-17 10:47:24 +0530

    dev commit

--- src/componenet/index.js
+++ src/componenet/index.js
@@ -1,25 +1,30 @@
-
-
-import React from 'react'
+import React from "react";
 
 export default class LightDarkMode extends React.Component {
   state = {
-    mode: 'Dark',
-  }
+    mode: "Light",
+  };
 
-  changeMode = e => {
-    this.setState(prevState => {
-      return {mode: prevState.mode==='Dark' ? 'Light' :'Dark'};
-    })
-  }
+  changeMode = (e) => {
+    this.setState((prevState) => {
+      return { mode: prevState.mode === "Dark" ? "Light" : "Dark" };
+    });
+  };
 
   render() {
-    const {mode} = this.state
+    const { mode } = this.state;
     return (
-      <div style={{backgroundColor: mode==="Dark" ? "#000":"#fff"}}>
-        <h1 style={{color: mode==="Dark" ? "#fff":"#000"}}>change to {mode} Mode</h1>
-        <button style={{border:"2px solid black",borderRadius:"8px"}} onClick={this.changeMode}>{mode} Mode</button>
+      <div style={{ backgroundColor: mode === "Dark" ? "#000" : "#fff" }}>
+        <h1 style={{ color: mode === "Dark" ? "#fff" : "#000" }}>
+          change to {mode} Mode
+        </h1>
+        <button
+          style={{ border: "2px solid black", borderRadius: "8px" }}
+          onClick={this.changeMode}
+        >
+          {mode} Mode
+        </button>
       </div>
-    )
+    );
   }
 }
